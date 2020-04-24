da4a25c7778ba72f33347b70eb36ae12a4c3b4d6
namitamanohar <manohar.namita@gmail.com>
2020-04-24 11:36:15 -0500

    styling for the bookstore

--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -34,7 +34,7 @@ namespace Bookstore
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
